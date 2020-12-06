36050c2f133a588198cd4518f8410568fd36dfc4
Psemp <57976914+Psemp@users.noreply.github.com>
2020-12-06 06:58:06 +0100

    added .js and modified HTML CSS

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+
+function alertButton(){
+    let question = document.getElementById("question").value;
+    answer = question.toUpperCase();
+    document.getElementById("question").value = answer;
+}
