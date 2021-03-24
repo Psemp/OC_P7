57886a1aed185c47aad1059b861e7d220dacc97c
Pierre Sempere <pierre.sempere.01@gmail.com>
2021-03-24 19:30:50 +0100

    question processing test

--- grandpyapp/tests/test_question_processing.py
+++ grandpyapp/tests/test_question_processing.py
@@ -1,10 +1,18 @@
-from grandpyapp.script.question_processing import question_processing
+import grandpyapp.script.question_processing as qp
 from grandpyapp.models.question import Question
 
-from grandpyapp.script.actions_on_str import string_modification
-from grandpyapp.script.actions_on_str import data_cleaning, identify_intent
-from grandpyapp.script.wikisearch import get_page_info, get_wiki_extract
-from grandpyapp.script.maps_search import get_coords, get_embed_map
+import grandpyapp.tests.mocks.mock_actions_on_str
+import grandpyapp.tests.mocks.mock_map_search
+import grandpyapp.tests.mocks.mock_wikisearch
+
+
+qp.string_modification = grandpyapp.tests.mocks.mock_actions_on_str.string_modification
+qp.data_cleaning = grandpyapp.tests.mocks.mock_actions_on_str.data_cleaning
+qp.identify_intent = grandpyapp.tests.mocks.mock_actions_on_str.identify_intent
+qp.get_page_info = grandpyapp.tests.mocks.mock_wikisearch.get_page_info
+qp.get_wiki_extract = grandpyapp.tests.mocks.mock_wikisearch.get_wiki_extract
+qp.get_coords = grandpyapp.tests.mocks.mock_map_search.get_coords
+qp.get_embed_map = grandpyapp.tests.mocks.mock_map_search.get_embed_map
 
 test_query = "Ou se trouve le Golden Gate ?"
 test_question = Question()
@@ -12,7 +20,7 @@ test_question = Question()
 
 def test_question_processing():
     """tests if formatting works, if 'embed map url' is in return --> intent identified correctly"""
-    test_html_return = question_processing(test_query, test_question)
+    test_html_return = qp.question_processing(test_query, test_question)
     print(test_html_return)
     assert "wikipedia page extract" in test_html_return
     assert "embed map url" in test_html_return
