c7c8e12721658b9aa8ffe6756eebd19d4dca2c51
Pierre Sempere <pierre.sempere.01@gmail.com>
2021-03-25 13:13:53 +0100

    pre deployment on Heroku

---
+++ config.py
@@ -0,0 +1,1 @@
+SECRET_KEY = 'L.:B/Cq\x0bH0dxMZJ\x0bkl%)OE\x0cR'
--- grandpyapp/views.py
+++ grandpyapp/views.py
@@ -4,6 +4,7 @@ from .models.question import Question
 
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 
 @app.route('/')
